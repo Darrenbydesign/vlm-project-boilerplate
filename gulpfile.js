656690ab6f0c12279eb5980c4a845d93c8cbff71
Darren Sorrels <dsorrels@valorem.com>
2018-03-21 09:35:58 -0500

    adds gulp sass task to convert sass to css

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,17 @@
+//  Plug-in Variable Declaration
 var gulp = require('gulp'),
     sass = require('gulp-sass');
+// Path Variable Declaration
+var paths = {
+
+  sassInput: 'app/sass/**/*.scss',
+  sassOutput: 'app/styles',
+};
+
+
+// Task Declaration
+gulp.task('sass', function(){
+  return gulp.src(paths.sassInput)
+  .pipe(sass())
+  .pipe(gulp.dest(paths.sassOutput))
+});
